f32a36604d6fa4254d2b92898d624b9fa032a149
osalamon <ondrej.salamon@vsb.cz>
2020-09-09 14:24:13 +0200

    another playing... with funcs

--- ahoj-svete/ahoj.go
+++ ahoj-svete/ahoj.go
@@ -14,8 +14,14 @@ func expon(x, y float64) float64 {
 	return math.Pow(x, y)
 }
 
+func prohodStringy(xStr, yStr string) (string, string) {
+	return yStr, xStr
+}
+
 func main() {
-	fmt.Println("Ahoj svete!")
+	ahoj := "Ahoj"
+	svete := "svete"
+	fmt.Printf("%s %s!\n", ahoj, svete)
 
 	random := rand.Intn(50)
 	fmt.Printf("Cislo %d zije!\n", random)
@@ -23,7 +29,7 @@ func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(20))
 
 	pi := math.Pi
-	fmt.Printf("Delka kruhu o r = 1 je: %g\n", pi)
+	fmt.Printf("Delka obvodu kruhu o r = 1 je: %g\n", pi)
 
 	xNr := 83275.547952374
 	yNr := 547402.503
@@ -32,4 +38,7 @@ func main() {
 	var x float64 = 2
 	var y float64 = 8
 	fmt.Printf("%g^%g=%g\n", x, y, expon(x, y))
+
+	ahoj, svete = prohodStringy(ahoj, svete)
+	fmt.Printf("%s %s!\n", ahoj, svete)
 }
